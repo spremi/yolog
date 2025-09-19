bb69db64b26e01386ecdfe57cf3c034b735234c4
Sanjeev Premi <spremi@ymail.com>
2025-09-20 01:22:11 +0530

    chore: s: Implement minimal app
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server/main.py
@@ -0,0 +1,68 @@
+#
+# yo!log
+#
+
+import asyncio
+import logging
+import os
+import uvicorn
+
+from fastapi import FastAPI
+
+
+PORT_OTLP_HTTP = int(os.getenv('PORT_OTLP_HTTP', 4318))
+PORT_OTLP_GRPC = int(os.getenv('PORT_OTLP_GRPC', 4317))
+
+PORT_UI = int(os.getenv('PORT_UI', 8000))
+
+#
+# Configure logger
+#
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger(__name__)
+
+#
+# OTLP Receiver
+#
+log_receiver: FastAPI = FastAPI(title='yo!log OTLP Receiver')
+
+#
+# LOG Provider
+#
+log_provider: FastAPI = FastAPI(title='yo!log LOG Provider')
+
+
+@log_provider.get('/')
+def provider_root() -> dict[str, str]:
+    return {'status': 'ok'}
+
+
+async def main() -> None:
+    """
+    Run 2 servers in parallel. Typical OTLP ports are 4137, 4138
+    - LOG server: Ingests OTLP logs.
+    - UI server : Provide logs to UI.
+    """
+
+    otlp_config = uvicorn.Config(log_receiver, host='0.0.0.0', port=PORT_OTLP_HTTP)
+    otlp_server = uvicorn.Server(otlp_config)
+
+    log_config = uvicorn.Config(log_provider, host='0.0.0.0', port=PORT_UI)
+    log_server = uvicorn.Server(log_config)
+
+    logger.info('Ready!')
+
+    # Run both concurrently
+    await asyncio.gather(
+        otlp_server.serve(),
+        log_server.serve()
+    )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
