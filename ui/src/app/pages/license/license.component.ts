6a9e97b53f5efb97f231279207fbf3f13298ec15
Sanjeev Premi <spremi@ymail.com>
2025-09-20 13:40:19 +0530

    feat: c: app: Add page '/license'
    Command:
      ng g component pages/license --standalone
    
    Somehow, option '--route license' is not working.
      Error: Unknown argument: route
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/pages/license/license.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-license',
+  imports: [],
+  templateUrl: './license.component.html',
+  styleUrl: './license.component.sass'
+})
+export class LicenseComponent {
+
+}
