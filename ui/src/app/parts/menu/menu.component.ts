6c6ba1eef80b73afc752f0b7d785652be619c360
Sanjeev Premi <spremi@ymail.com>
2025-09-20 13:55:13 +0530

    feat: c: parts: Add 'menu'
    Command:
      ng generate component parts/menu --standalone
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/parts/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-menu',
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.sass'
+})
+export class MenuComponent {
+
+}
