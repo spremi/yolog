78fe3b0a00e37192bc308292f8d127f0c2af34ad
Sanjeev Premi <spremi@ymail.com>
2025-09-20 13:38:55 +0530

    feat: c: app: Add page '/settings'
    Command:
        ng g component pages/settings --standalone
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/pages/settings/settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-settings',
+  imports: [],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.sass'
+})
+export class SettingsComponent {
+
+}
