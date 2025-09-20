8084651a79913e67c2cd190753af5c6fc6928289
Sanjeev Premi <spremi@ymail.com>
2025-09-20 13:48:36 +0530

    feat: c: app: Add routes
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./pages/settings/settings.component').then((m) => m.SettingsComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'license',
+    loadComponent: () =>
+      import('./pages/license/license.component').then((m) => m.LicenseComponent),
+    data: {
+      animation: 'license'
+    },
+  },
+  {
+    path: 'error',
+    loadComponent: () =>
+      import('./pages/error/error.component').then((m) => m.ErrorComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
