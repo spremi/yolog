dd641e547ee5e148086ce6efcea052c501f3b04f
Sanjeev Premi <spremi@ymail.com>
2025-09-21 01:47:53 +0530

    feat: c: parts: menu: Basic implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/parts/menu/menu.component.ts
+++ ui/src/app/parts/menu/menu.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'sp-menu',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgOptimizedImage,
+],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.sass'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit, OnDestroy {
+  private SLIDE_DURATION_MS = 300;
+  private IDLE_TIMEOUT_MS = 5000;
 
+  private router = inject(Router);
+
+  private routeSub: Subscription | null = null;
+
+  activeMenu: string | null = null;
+
+  activeRoute = '';
+  logPaused = false;
+
+  ngOnInit(): void {
+    this.routeSub = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe(ev => {
+        this.activeRoute = ev.url;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe();
+    this.routeSub = null;
+  }
 }
