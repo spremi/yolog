66c7c3aee4a0b008cf0bd688e5a4e04fbd071a37
Sanjeev Premi <spremi@ymail.com>
2025-09-20 13:37:34 +0530

    feat: c: app: Add page '/about'
    Command:
      ng g component pages/about --standalone
    
    Somehow, option '--route about' is not working.
      Error: Unknown argument: route
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/pages/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-about',
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.sass'
+})
+export class AboutComponent {
+
+}
