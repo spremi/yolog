10682be26defde5af737f058249e6ef01b4f1188
Sanjeev Premi <spremi@ymail.com>
2025-09-21 01:19:00 +0530

    feat: c: app: Implement basic layout
    Planning to keep menu position configurable.
    ... have a different kind of layout in mind.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { BodyComponent } from "@parts/body/body.component";
+import { MenuComponent } from '@parts/menu/menu.component';
 
 @Component({
   selector: 'sp-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BodyComponent,
+    MenuComponent,
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
+  host: {
+    '[class.menu-left]': `menuPosition === 'left'`
+  }
 })
 export class AppComponent {
-  title = 'yoLOG';
+  @Input() menuPosition: 'left' | 'right' = 'right';
+
+  title = 'yo!LOG';
 }
