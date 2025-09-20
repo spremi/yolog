1f7a5fd220c3ea7b2bfdd73ff0177c28d98891bf
Sanjeev Premi <spremi@ymail.com>
2025-09-20 14:00:54 +0530

    feat: c: parts: Add 'body'
    Command:
      ng generate component parts/body --standalone
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/parts/body/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-body',
+  imports: [],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.sass'
+})
+export class BodyComponent {
+
+}
