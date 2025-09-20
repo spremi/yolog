6ac4574f861dbe669a12443e38c73d52ea0d726f
Sanjeev Premi <spremi@ymail.com>
2025-09-21 02:08:57 +0530

    feat: c: pages: license: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/pages/license/license.component.ts
+++ ui/src/app/pages/license/license.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { NgOptimizedImage } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sp-license',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './license.component.html',
   styleUrl: './license.component.sass'
 })
 export class LicenseComponent {
+  private readonly router = inject(Router);
 
+  public goHome(): void {
+    this.router.navigate(['/home']);
+  }
 }
