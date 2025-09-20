fa06d97523fdee38e396f6baabbfb4ef4efbbb35
Sanjeev Premi <spremi@ymail.com>
2025-09-21 01:23:42 +0530

    feat: c: svc: settings: Add 'menuPosition'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/services/settings.service.ts
+++ ui/src/app/services/settings.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Injectable, signal, Signal } from '@angular/core';
+
+import { MenuPosition } from '@base/app.types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
+  private _menuPosition = signal<MenuPosition>('right');
 
   constructor() { }
+
+  public getMenuPosition(): Signal<MenuPosition> {
+    return this._menuPosition.asReadonly();
+  }
+
+  public toggleMenuPosition(): void {
+    const nextPos = (this._menuPosition() === 'left' ? 'right' : 'left');
+
+    this._menuPosition.set(nextPos);
+  }
 }
