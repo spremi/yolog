fab77189dee34e7355b63d9ffefb20442a7f64cf
Sanjeev Premi <spremi@ymail.com>
2025-09-21 02:01:37 +0530

    feat: c: pages: about: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/pages/about/about.component.ts
+++ ui/src/app/pages/about/about.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'sp-about',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.sass'
 })
 export class AboutComponent {
+  private readonly router = inject(Router);
 
+  public goHome(): void {
+    this.router.navigate(['/home']);
+  }
 }
