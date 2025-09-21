d241f6c9bcf1539bc77785017160183be222c52b
Sanjeev Premi <spremi@ymail.com>
2025-09-21 13:21:04 +0530

    feat: c: svc: state: Add toggle to control logging
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/services/state.service.ts
+++ ui/src/app/services/state.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
 
+
+import { Injectable, signal } from '@angular/core';
+
+/**
+ * Encapsulates runtime state of the application.
+ *
+ * State is derived from persistent settings... but not preserved.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+  /**
+   * Logs are paused?
+   */
+  private logsPaused = signal(false);
 
   constructor() { }
+
+  /**
+   * Is logging paused?
+   */
+  public isLogPaused(): boolean {
+    return this.logsPaused();
+  }
+
+  /**
+   * Toggle state of logging.
+   */
+  public toggleLogging(): void {
+    const current = this.logsPaused();
+
+    this.logsPaused.set(!current);
+  }
 }
