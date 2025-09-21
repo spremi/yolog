6bee18f96ec67a406659170ef8e9d4683f0ebdea
Sanjeev Premi <spremi@ymail.com>
2025-09-21 23:00:00 +0530

    feat: c: svc: log: Initial implementation
    Read logs from websocket and normalize them.
    
    Also add default values that can be used in across
    the application.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/app.defaults.ts
@@ -0,0 +1,27 @@
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+//
+// Default values used across the application.
+// All definitions are explicit - to avoid possible circular dependency.
+//
+
+/**
+ * See: LOG_LEVELS
+ */
+export const DEFAULT_LOG_LEVEL = 3;   // INFO
+
+/**
+ * See: LOG_COLUMNS
+ */
+export const DEFAULT_LOG_COLUMNS = ['ts', 'message'];
+
+/**
+ * Number of logs to keep.
+ */
+export const DEFAULT_LOG_COUNT = 500;
--- ui/src/app/services/log.service.ts
+++ ui/src/app/services/log.service.ts
@@ -1,9 +1,107 @@
-import { Injectable } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { inject, Injectable, Signal, signal } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { v4 as uuidv4 } from 'uuid';
+
+import { DEFAULT_LOG_COUNT, DEFAULT_LOG_LEVEL } from '@base/app.defaults';
+import { LogEntry } from '@base/app.types';
+import { getNormalizedKey, LOG_KEYS } from '@base/models/log-column';
+
+import { StateService } from './state.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
 
-  constructor() { }
+  private stateSvc = inject(StateService);
+
+  private ws$: WebSocketSubject<LogEntry>;
+
+  private ws_uri = 'ws://localhost:8000/view/ws';
+
+  private sub: Subscription;
+
+  /**
+   * Incoming logs - after normalization.
+   */
+  private _logs = signal<LogEntry[]>([]);
+
+
+  constructor() {
+    this.ws$ = webSocket<LogEntry>(this.ws_uri);
+
+    this.sub = this.ws$.subscribe({
+      next: (arg) => this.onNewLog(arg),
+      error: (err) => console.error('WebSocket error:', err),
+      complete: () => console.log('WebSocket closed'),
+    });
+  }
+
+  public getLogs(): Signal<LogEntry[]> {
+    return this._logs.asReadonly();
+  }
+
+  /**
+   * Encapsulates actions to be performed when new log is received.
+   */
+  private onNewLog(log: LogEntry) {
+    if (this.stateSvc.isLogPaused()) {
+      return;
+    }
+
+    const normalized = this.normalizeLog(log);
+
+    this._logs.update((rows) => {
+      // Add new log to top
+      const newLogs = [normalized, ...rows];
+
+      // Ensure we don't cross maximum log count.
+      // TODO: Replace with configurable value.
+      const maxCount = DEFAULT_LOG_COUNT
+
+      return newLogs.length > maxCount ? newLogs.slice(0, maxCount) : newLogs;
+    });
+  }
+
+  /**
+   * Normalize log entry.
+   */
+  private normalizeLog(log: LogEntry): LogEntry {
+    let normalized: LogEntry = {} as LogEntry;
+
+    //
+    // Normalize all keys.
+    //
+    for (const attr in log) {
+      const normKey = getNormalizedKey(attr) || attr;
+
+      normalized[normKey] = log[attr];
+    }
+
+    //
+    // Normalize log level.
+    //
+    const levelKey = LOG_KEYS.LEVEL;
+
+    normalized[levelKey] = (normalized[levelKey] ?? DEFAULT_LOG_LEVEL)
+                              .toLocaleUpperCase();
+
+    //
+    // Add unique ID.
+    //
+    normalized[LOG_KEYS.UID] = uuidv4();
+
+    return normalized;
+  }
 }
