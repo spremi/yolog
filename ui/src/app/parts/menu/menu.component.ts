62fe43b17b234dedc618bcb210c3f65a04212d49
Sanjeev Premi <spremi@ymail.com>
2025-09-27 16:52:46 +0530

    feat: c: parts: menu: Toggle menu panel
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/parts/menu/menu.component.ts
+++ ui/src/app/parts/menu/menu.component.ts
@@ -8,12 +8,23 @@
 
 
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { filter, Subscription } from 'rxjs';
+import { filter, fromEvent, merge, of, Subscription, switchMap, timer } from 'rxjs';
 
+import { MenuPosition } from '@base/app.types';
 import { SettingsService } from '@base/services/settings.service';
 import { StateService } from '@base/services/state.service';
+
+/**
+ * Different states of menu panel - required during transitions.
+ */
+enum PanelState {
+  CLOSED = 1,
+  CLOSING = 10,
+  OPEN = 2,
+  OPENING = 20,
+}
 
 @Component({
   selector: 'sp-menu',
@@ -34,11 +45,17 @@ export class MenuComponent implements OnInit, OnDestroy {
   private stateSvc = inject(StateService);
 
   private routeSub: Subscription | null = null;
+  private idleSub: Subscription | null = null;
 
   activeMenu: string | null = null;
+  nextMenu: string | null = null;
 
   activeRoute = '';
   logPaused = false;
+
+  panelState = PanelState.CLOSED;
+
+  @ViewChild('panelRef') panelRef!: ElementRef<HTMLElement>;
 
   ngOnInit(): void {
     this.routeSub = this.router.events
@@ -53,6 +70,9 @@ export class MenuComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.routeSub?.unsubscribe();
     this.routeSub = null;
+
+    this.idleSub?.unsubscribe();
+    this.idleSub = null;
   }
 
   /**
@@ -60,6 +80,14 @@ export class MenuComponent implements OnInit, OnDestroy {
    */
   go(target: string): void {
     this.router.navigate([`/${target}`]);
+  }
+
+  /**
+   * Get menu position.
+   */
+  getPosition(): MenuPosition {
+    const pos = this.settingSvc.getMenuPosition();
+    return pos();
   }
 
   /**
@@ -76,5 +104,107 @@ export class MenuComponent implements OnInit, OnDestroy {
     this.stateSvc.toggleLogging();
 
     this.logPaused = this.stateSvc.isLogPaused();
+  }
+
+  /**
+   * Toggle panel - open/ close.
+   */
+  togglePanel(menu: string) {
+    if (this.panelState === PanelState.OPENING ||
+        this.panelState === PanelState.CLOSING) {
+      // No action
+      return;
+    }
+
+    if (this.activeMenu === menu) {
+      // Same menu clicked.
+      this.closeMenu();
+
+      return;
+    }
+
+    if (this.panelState === PanelState.OPEN) {
+      // Close current menu and save 'chosen' menu.
+      this.nextMenu = menu;
+      this.closeMenu();
+
+      return;
+    }
+
+    // No active menu. Open 'chosen' menu.
+    this.openMenu(menu);
+  }
+
+  /**
+   * Open specified menu.
+   * It auto closes after inactivity.
+   */
+  private openMenu(menu: string) : void {
+    const panel = this.panelRef.nativeElement;
+
+    this.activeMenu = menu;
+
+    panel.classList.add('open');
+    this.panelState = PanelState.OPENING;
+
+    this.startIdleTimer();
+
+    setTimeout(() => {
+      if (this.panelState === PanelState.OPENING) {
+        this.panelState = PanelState.OPEN;
+      }
+    }, this.SLIDE_DURATION_MS);
+  }
+
+  /**
+   * Close active menu.
+   * Optionally, open 'next' menu.
+   */
+  closeMenu() : void {
+    const panel = this.panelRef.nativeElement;
+
+    panel.classList.remove('open');
+    this.panelState = PanelState.CLOSING;
+
+    this.stopIdleTimer();
+
+    setTimeout(() => {
+      if (this.panelState !== PanelState.CLOSING) {
+        return;
+      }
+
+      this.activeMenu = null;
+      this.panelState = PanelState.CLOSED;
+
+      if (this.nextMenu) {
+        // Open next menu
+        const menu = this.nextMenu;
+
+        this.nextMenu = null;
+        this.openMenu(menu);
+      }
+    }, this.SLIDE_DURATION_MS);
+  }
+
+  private startIdleTimer(): void {
+    const panel = this.panelRef.nativeElement;
+
+    this.idleSub = merge(
+        of(null),
+        fromEvent(panel, 'click')
+      )
+      .pipe(
+        switchMap(() => {
+          return timer(this.IDLE_TIMEOUT_MS);
+        })
+      )
+      .subscribe(() => {
+        this.closeMenu();
+      });
+  }
+
+  private stopIdleTimer(): void {
+    this.idleSub?.unsubscribe();
+    this.idleSub = null;
   }
 }
