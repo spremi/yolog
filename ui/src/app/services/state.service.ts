032172b38e8a1d4c4552f9489e63925edb14a0d5
Sanjeev Premi <spremi@ymail.com>
2025-09-27 21:55:19 +0530

    feat: c: svc: state: Implement dynamic state
    The dynamic (runtime) state is not persistent.
    
    It derives from persistent settings, but isn't saved.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/services/state.service.ts
+++ ui/src/app/services/state.service.ts
@@ -7,7 +7,14 @@
 //
 
 
-import { Injectable, signal } from '@angular/core';
+import {
+  effect, inject, Injectable, Signal, signal, untracked, WritableSignal
+} from '@angular/core';
+
+import { MenuPosition } from '@base/app.types';
+import { AppSettings } from '@base/models/app-settings';
+
+import { SettingsService } from './settings.service';
 
 /**
  * Encapsulates runtime state of the application.
@@ -18,12 +25,44 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root'
 })
 export class StateService {
+  private settingSvc = inject(SettingsService);
+
+  private _settings: Signal<AppSettings>;
+
+  private _menuPosition: WritableSignal<MenuPosition>;
+  private _logLevel: WritableSignal<number>;
+  private _logCount: WritableSignal<number>;
+  private _viewColumns: WritableSignal<string[]>;
+
   /**
    * Logs are paused?
    */
   private logsPaused = signal(false);
 
-  constructor() { }
+  constructor() {
+    this._settings = this.settingSvc.getAppSettings();
+
+    const initial = this._settings();
+
+    this._menuPosition = signal(initial.menuPosition);
+    this._logLevel = signal(initial.logLevel);
+    this._logCount = signal(initial.logCount);
+    this._viewColumns = signal(initial.viewColumns);
+
+    effect(() => {
+      // Read the 'signal'.
+      // This will become a dependency for effect.
+      const update = this._settings();
+
+      untracked(() => {
+        // Signals here won't become dependencies for effect().
+        this._menuPosition.set(update.menuPosition);
+        this._logLevel.set(update.logLevel);
+        this._logCount.set(update.logCount);
+        this._viewColumns.set(update.viewColumns);
+      });
+    });
+  }
 
   /**
    * Is logging paused?
@@ -39,5 +78,39 @@ export class StateService {
     const current = this.logsPaused();
 
     this.logsPaused.set(!current);
+  }
+
+  public getMenuPosition(): Signal<MenuPosition> {
+    return this._menuPosition.asReadonly();
+  }
+
+  public toggleMenuPosition(): void {
+    const nextPos = (this._menuPosition() === 'left' ? 'right' : 'left');
+
+    this._menuPosition.set(nextPos);
+  }
+
+  public getLogLevel(): Signal<number> {
+    return this._logLevel.asReadonly();
+  }
+
+  public setLogLevel(level: number): void {
+    this._logLevel.set(level);
+  }
+
+  public getLogCount(): Signal<number> {
+    return this._logCount.asReadonly();
+  }
+
+  public setLogCount(count: number): void {
+    this._logCount.set(count);
+  }
+
+  public getViewColumns(): Signal<string[]> {
+    return this._viewColumns.asReadonly();
+  }
+
+  public setViewColumns(columns: string[]): void {
+    this._viewColumns.set(columns);
   }
 }
