ed2ae19bedd3fae044911a343610f781b5ba79e0
Sanjeev Premi <spremi@ymail.com>
2025-09-27 20:19:11 +0530

    feat: c: models: Define AppSettings
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/models/app-settings.ts
+++ ui/src/app/models/app-settings.ts
@@ -1,2 +1,34 @@
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+import { MenuPosition } from '@base/app.types';
+
+/**
+ * Collection of all settings that are explicitly saved by the user.
+ */
 export interface AppSettings {
+  /**
+   * Position of menu bar.
+   */
+  menuPosition: MenuPosition;
+
+  /**
+   * Filter for log level.
+   */
+  logLevel: number;
+
+  /**
+   * Number of log lines to keep.
+   */
+  logCount: number;
+
+  /**
+   * Visible columns.
+   */
+  viewColumns: string[];
 }
