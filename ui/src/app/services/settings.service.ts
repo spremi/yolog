c0bb5e24bc22bad5a4e3f37444f2e0edad41f507
Sanjeev Premi <spremi@ymail.com>
2025-09-27 18:41:47 +0530

    feat: c: svc: settings: Persist log level
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/services/settings.service.ts
+++ ui/src/app/services/settings.service.ts
@@ -9,7 +9,7 @@
 
 import { Injectable, signal, Signal, WritableSignal } from '@angular/core';
 
-import { DEFAULT_MENU_POSITION } from '@base/app.defaults';
+import { DEFAULT_LOG_LEVEL, DEFAULT_MENU_POSITION } from '@base/app.defaults';
 import { MenuPosition } from '@base/app.types';
 
 @Injectable({
@@ -17,12 +17,18 @@ import { MenuPosition } from '@base/app.types';
 })
 export class SettingsService {
   private readonly K_MENU = 'menu';
+  private readonly K_LOG_LEVEL = 'level';
 
   private _menuPosition: WritableSignal<MenuPosition>;
+  private _logLevel: WritableSignal<number>;
+
 
   constructor() {
     const menu = this.readStore<MenuPosition>(this.K_MENU, DEFAULT_MENU_POSITION);
     this._menuPosition = signal(menu);
+
+    const logLevel = this.readStore<number>(this.K_LOG_LEVEL, DEFAULT_LOG_LEVEL);
+    this._logLevel = signal(logLevel);
   }
 
   public getMenuPosition(): Signal<MenuPosition> {
@@ -35,6 +41,16 @@ export class SettingsService {
     this._menuPosition.set(nextPos);
 
     localStorage.setItem(this.K_MENU, JSON.stringify(nextPos));
+  }
+
+  public getLogLevel(): number {
+    return this._logLevel();
+  }
+
+  public setLogLevel(level: number) {
+    this._logLevel.set(level);
+
+    localStorage.setItem(this.K_LOG_LEVEL, level.toString());
   }
 
   /**
