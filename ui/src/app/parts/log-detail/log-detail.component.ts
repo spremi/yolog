f781f637b841aba918cba302e48086fbaf93306b
Sanjeev Premi <spremi@ymail.com>
2025-12-27 19:38:08 +0530

    chore: c: app: Add component 'log-detail'
    Command:
      ng g component parts/log-detail --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/parts/log-detail/log-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-log-detail',
+  imports: [],
+  templateUrl: './log-detail.component.html',
+  styleUrl: './log-detail.component.sass'
+})
+export class LogDetailComponent {
+
+}
