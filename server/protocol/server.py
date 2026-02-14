a681c8e430ac7ab4e28be0382da2c7a6d7bd1b47
Sanjeev Premi <spremi@ymail.com>
2026-02-15 01:54:59 +0530

    feat: s: protocol: Create gRPC server
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server/protocol/server.py
@@ -0,0 +1,35 @@
+#
+# yo!log
+#
+
+import logging
+import grpc
+import os
+
+from opentelemetry.proto.collector.logs.v1 import logs_service_pb2_grpc
+
+from protocol.service import LogsServiceAsync
+
+PORT_OTLP_GRPC = int(os.getenv('PORT_OTLP_GRPC', 4317))
+
+logger = logging.getLogger(__name__)
+
+
+async def grpc_aio_server() -> None:
+    """
+    Starts an asynchronous gRPC server to receive OpenTelemetry logs.
+    """
+
+    server = grpc.aio.server()
+
+    # Register 'LogsServiceAsync' as gRPC service handler.
+    logs_service_pb2_grpc.add_LogsServiceServicer_to_server(LogsServiceAsync(), server)
+
+    # Bind server to the specified port.
+    server.add_insecure_port(f'0.0.0.0:{PORT_OTLP_GRPC}')
+
+    logger.info(f'Listening on gRPC port {PORT_OTLP_GRPC}')
+
+    await server.start()
+
+    await server.wait_for_termination()
