ecceca207c38c67b49ffaf57f52a0549adc90d6c
Sanjeev Premi <spremi@ymail.com>
2026-02-15 15:33:31 +0530

    feat: c: app: Add pipe 'tsFormat'
    Command:
      ng g pipe pipes/ts-format
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ ui/src/app/pipes/ts-format.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TsFormatPipe } from './ts-format.pipe';
+
+describe('TsFormatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TsFormatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
