8c40f328e6cc91bbd0c3d59ad63878c4470c64e7
Sanjeev Premi <spremi@ymail.com>
2026-02-15 18:15:27 +0530

    feat: c: parts: log-detail: Use 'ts-format' pipe
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/parts/log-detail/log-detail.component.ts
+++ ui/src/app/parts/log-detail/log-detail.component.ts
@@ -1,10 +1,20 @@
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
 import { Component, Input } from '@angular/core';
 import { LogEntry } from '@base/app.types';
 import { LOG_KEYS, LOG_LABELS } from '@base/models/log-column';
+import { TsFormatPipe } from '@pipes/ts-format.pipe';
 
 @Component({
   selector: 'sp-log-detail',
-  imports: [],
+  imports: [TsFormatPipe],
   templateUrl: './log-detail.component.html',
   styleUrl: './log-detail.component.sass'
 })
