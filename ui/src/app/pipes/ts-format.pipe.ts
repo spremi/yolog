adf508bf2435a58578f2698d6c2b102b33a78513
Sanjeev Premi <spremi@ymail.com>
2026-02-15 17:58:12 +0530

    feat: c: pipes: ts-format: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- ui/src/app/pipes/ts-format.pipe.ts
+++ ui/src/app/pipes/ts-format.pipe.ts
@@ -1,12 +1,24 @@
-import { Pipe, PipeTransform } from '@angular/core';
+//
+// [yo-log]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { TsFormatterService } from '@base/services/ts-formatter.service';
 
 @Pipe({
-  name: 'tsFormat'
+  name: 'tsFormat',
+  pure: true          // Recompute only when input changes.
 })
 export class TsFormatPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  private formatSvc = inject(TsFormatterService);
 
+  transform(iso: string): string {
+    return this.formatSvc.format(iso);
+  }
 }
